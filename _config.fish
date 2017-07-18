1a90d300be5f82670b7d6bd573fe26218bd2efb9
bisque33 <bisque33@gmail.com>
2017-07-18 11:58:49 +0900

    fishのconfigを修正

--- _config.fish
+++ _config.fish
@@ -1,6 +1,8 @@
 # PATH
 # set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 
+set -x EDITOR vim
+
 # peco
 function fish_user_key_bindings
     bind \cr peco_select_history
@@ -14,11 +16,22 @@ balias be 'bundle exec'
 balias docc 'docker-compose'
 balias kube 'kubectl'
 balias vim 'nvim'
+balias awslogs lazy-awslogs
 
 # anyenv 非対応
 # set -x PATH "$HOME/.anyenv/bin $PATH"
 # eval (anyenv init - fish)
 
-# goenv
+# goenv 非対応
 # goenv init - | psub
 # source (goenv init - | psub)
+
+# direnv
+eval (direnv hook fish)
+
+# go
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
+# lazy-awslogs
+. $HOME/go/src/github.com/cm-igarashi-ryosuke/lazy-awslogs/shell-completion/lazy-awslogs.fish
