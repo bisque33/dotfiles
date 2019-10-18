ce2b9c2e6dd1dfc2544472c4f209368bd6ad7058
bisque33 <bisque33@gmail.com>
2019-10-19 01:49:07 +0900

    switch xxxenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,16 +13,8 @@ balias be 'bundle exec'
 balias d  'docker'
 balias dc 'docker-compose'
 
-# anyenv 非対応
-# set -x PATH "$HOME/.anyenv/bin $PATH"
-# eval (anyenv init - fish)
-
-# goenv 非対応
-# goenv init - | psub
-# source (goenv init - | psub)
-
-# nodebrew
-set -x PATH $HOME/.nodebrew/current/bin $PATH
+# anyenv
+status --is-interactive; and . (anyenv init - | psub)
 
 # direnv
 eval (direnv hook fish)
@@ -32,7 +24,7 @@ set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 
 # gcp
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+. /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 
 # lazy-awslogs
 . $HOME/go/src/github.com/cm-igarashi-ryosuke/lazy-awslogs/shell-completion/lazy-awslogs.fish
